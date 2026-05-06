6b7ae72d8e64367d719c11b76d5307bee68d58b6
Cuber01 <johnmallard631@gmail.com>
2026-05-06 22:16:31 +0200

    Added compass

---
+++ Assets/Scripts/UI/Compass.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class Compass : MonoBehaviour
+{
+    [SerializeField] private Transform playerTransform;
+    [SerializeField] private RectTransform arrowRect;
+    
+    private Transform exitPoint;
+
+    public void Awake()
+    {
+        exitPoint = GameObject.Find("FinishZone").transform;
+    }
+
+    void Update()
+    {
+        Vector3 worldDirection = exitPoint.position - playerTransform.position;
+        Vector3 localDirection = playerTransform.InverseTransformDirection(worldDirection);
+        float angle = Mathf.Atan2(localDirection.z, localDirection.x) * Mathf.Rad2Deg;
+        arrowRect.rotation = Quaternion.Euler(0, 0, angle);
+    }
+    
+}
