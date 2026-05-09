d1f6a2e35a5523fc6dafef71bc4e8e108cf66c3a
bnm <57193455+kyanbasu@users.noreply.github.com>
2026-05-09 15:32:45 +0200

    fix duplicates from merge

--- Assets/Scripts/EnemyAI/EnemyMovement.cs
+++ Assets/Scripts/EnemyAI/EnemyMovement.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
+using EnemyAI;
 
 namespace EnemySystem
 {
--- Assets/Scripts/PlayerShootingSystem/Nade.cs
+++ Assets/Scripts/PlayerShootingSystem/Nade.cs
@@ -66,49 +66,16 @@ public class Nade : MonoBehaviour, IThrowable
             }
         }
     }
-    
-     
+
+
     IEnumerator CookCoroutine(float blastRadius, int flatDamage, float delay, LayerMask damageLayerMask)
     {
         yield return new WaitForSecondsRealtime(delay);
-        Explode(blastRadius,flatDamage, damageLayerMask);
+        Explode(blastRadius, flatDamage, damageLayerMask);
         yield return new WaitForSecondsRealtime(2f);
         Destroy(gameObject);
     }
 
-
-    void Explode(float blastRadius, int flatDamage, LayerMask damageLayerMask)
-    {
-        Vector3 explosionPoint=transform.position;
-        Collider[] hitColliders=Physics.OverlapSphere(explosionPoint,blastRadius,damageLayerMask,QueryTriggerInteraction.Ignore);
-        if (explosionParticle)
-        {
-            explosionParticle.gameObject.transform.parent = null;
-            explosionParticle.Play();
-        }
-
-        if (explosionSound)
-        {
-            explosionSound.Play();
-        }
-        GetComponent<MeshRenderer>().enabled = false;
-        foreach (Collider col in hitColliders)
-        {
-     
-            if (col.TryGetComponent<ICharacter>(out var damageable))
-            {
-                //float distance = Vector3.Distance(explosionPoint, col.bounds.center);
-                //float falloff = Mathf.Clamp01(distance / blastRadius); // 0 → close, 1 → edge
-                float finalDamage = flatDamage;
-                 
-                //Debug.Log(col.name + ", distance: " + distance + ", finalDamage: " + finalDamage);
-                 
-                damageable.Damage(finalDamage);
-            }
-        }
-
-    }
-      
     public void Cook()
     {
     }
